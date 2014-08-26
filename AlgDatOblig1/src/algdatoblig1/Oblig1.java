a28802134abf9a219c1e6b0914fa6ee77b317012
madsmkarlstad <mads.karlstad@me.com>
2014-08-26 12:10:17 +0200

    Tester litt

--- AlgDatOblig1/src/algdatoblig1/Oblig1.java
+++ AlgDatOblig1/src/algdatoblig1/Oblig1.java
@@ -1,15 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package algdatoblig1;
 
-/**
- *
- * @author madsmkarlstad
- */
-public class Oblig1 {
+/*
+* Skrevet av:
+* Mads Myrbakken Karlstad, studentnr: 193949
+* Erlend Westbye, studentnr: 193377
+* William Benjamin Wold, studentnr:
+* Christoffer Baier Jønsberg, studentnr:
+*/
+import java.util.*;
+import javax.swing.*;
 
+public class Oblig1{
+    
+    public static int maks(int[] a){
+        if(a.length < 1)
+            throw new java.util.NoSuchElementException("a er tom");
+        
+        int m = 0;
+
+        for(int i = 1; i<a.length; i++){
+            if(a[i] > a[m]) m = i;
+        }
+        JOptionPane.showMessageDialog(null,m);
+        return m;
+    }
 }
+
+
+
+
+
+
