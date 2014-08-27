7e76c4f69943201c8b46d8617a38960b01167bd0
madsmkarlstad <mads.karlstad@me.com>
2014-08-27 13:26:26 +0200

    oppgave 1-4 er done

--- AlgDatOblig1/src/algdatoblig1/Oblig1.java
+++ AlgDatOblig1/src/algdatoblig1/Oblig1.java
@@ -13,22 +13,109 @@ import javax.swing.*;
 
 public class Oblig1{
     
+    //Oppgave 1
     public static int maks(int[] a){
-        if(a.length < 1)
-            throw new java.util.NoSuchElementException("a er tom");
-        
-        int m = 0;
 
-        for(int i = 1; i<a.length; i++){
-            if(a[i] > a[m]) m = i;
+        for(int i = 0; i < a.length-1; i++){
+            
+            if(a[i] > a[i+1]){
+
+                int større = a[i];
+                int mindre = a[i+1];
+
+                a[i+1] = større;
+                a[i] = mindre;
+
+            }
         }
-        JOptionPane.showMessageDialog(null,m);
-        return m;
+        return a[a.length-1];
+    }
+    public static boolean erSortert(int[] a){
+        for(int i=0;i<a.length-1;i++){
+            if(a[i]>a[i+1]){
+                return false;
+            }          
+        }
+        return true;
+    }
+    
+    //Oppgave 2
+    public static void sortering(int[] a){
+
+        boolean sortert = false;
+        int sistepost = 1;
+
+        while (!sortert) {
+
+            for (int i = 0; i < a.length - sistepost; i++) {
+
+                if (a[i] > a[i + 1]) {
+
+                    int større = a[i];
+                    int mindre = a[i + 1];
+
+                    a[i + 1] = større;
+                    a[i] = mindre;
+
+                    System.out.println(Arrays.toString(a));
+                }
+            }
+
+            sistepost++;
+
+            if(sistepost == 10){
+
+                sortert = true;
+            }
+        }
+    }
+    
+    //Oppgave 3
+    public static int antallUlikeSortert(int[] a){ 
+        
+        int[] array = a;
+        
+        erSortert(array);
+        
+        boolean sortert = erSortert(array);
+        int forekomster=0;
+
+        if(!sortert)
+            throw new IllegalStateException("Tabellen er ikke sortert");
+        if(sortert){
+            if(a.length == 0){
+                forekomster = 0;
+            }
+            if(a.length > 0){
+                forekomster=1;
+                for(int i = 0;i<a.length-1;i++){
+                    if(a[i]!=a[i+1]){
+                        forekomster++;
+                    }
+                }
+            }
+        }
+        System.out.println(forekomster);        
+        return forekomster;
+    }
+    
+    //Oppgave 4
+    public static int antallUlikeUsortert(int[] a){
+        int forekomster=0;
+        
+        if(a.length==0){
+            forekomster=0;
+        }
+        if(a.length>0){
+            forekomster=1;
+            for(int i=0;i<a.length-1;i++){
+                int sjekk = a[i];
+                if(sjekk!=a[i++]){
+                    forekomster++;
+                }
+            }
+        }
+        System.out.println(forekomster);
+        return forekomster;
     }
 }
-
-
-
-
-
-
--- AlgDatOblig1/src/algdatoblig1/Oblig1Driver.java
+++ AlgDatOblig1/src/algdatoblig1/Oblig1Driver.java
@@ -16,8 +16,8 @@ public class Oblig1Driver {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int[]a={0,1,2,3,4,5,6,7,8,9};
-        Oblig1.maks(a);
+        int[]a={0,1,1,2,3,4,4,5,6};
+        Oblig1.antallUlikeUsortert(a);
     }
     
 }
